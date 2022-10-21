0f65f8fa3e346833b6c4b0112c759e183555a295
karim15084 <karimahsalaeh150843@gmail.com>
2022-10-21 11:50:46 +0700

    ส่งไฟล์comment

---
+++ comment.php
@@ -0,0 +1,15 @@
+<?php
+
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+ echo "Karimah Salaeh<br>";
+ echo "Information Technology<br>";
+ //echo "Pattani<br>";
+ #echo "Thailand<br>";
+
+ //บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+ /* echo "<br>";
+ echo "94000<br>";
+ echo "Asia<br>"; */
+ echo "Universal";
+
+ ?>
