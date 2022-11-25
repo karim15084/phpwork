a4c5df4716ca78d23a43674be58cdd343fac0f78
karim15084 <karimahsalaeh150843@gmail.com>
2022-11-25 17:22:08 +0700

    25/11/65

---
+++ foreach.php
@@ -0,0 +1,10 @@
+<?php
+
+  $studend = array("Bright","Win","Nanon","Tay","Mick");
+  foreach($student as $std){
+    echo "<span style='color: red;'>";
+    echo $std;
+    echo "</span><br>";
+}
+
+?>
